5c3f4925caef5776b82092ba90732a4d634f4613
lutfi-hakim <21lutfihakim@gmail.com>
2021-01-14 02:39:25 +0700

    update page & perpage

--- src/routers/blog.js
+++ src/routers/blog.js
@@ -1,23 +1,39 @@
-const express = require('express');
-const { body } = require('express-validator');
+const express = require("express");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
-const blogController = require('../controllers/blog');
+const blogController = require("../controllers/blog");
 
-router.post('/post', [
-    body('title').isLength({ min: 5 }).withMessage('input title min 5 karakter'),
-    body('body').isLength({ min: 5 }).withMessage('input content min 5 karakter')],
-    blogController.createBlogPost);
+router.post(
+  "/post",
+  [
+    body("title")
+      .isLength({ min: 5 })
+      .withMessage("input title min 5 karakter"),
+    body("body")
+      .isLength({ min: 5 })
+      .withMessage("input content min 5 karakter"),
+  ],
+  blogController.createBlogPost
+);
 
-router.get('/posts', blogController.getAllBlogPost);
+router.get("/posts", blogController.getAllBlogPost);
 
-router.get('/post/:postId', blogController.getByIdBlogPost);
-router.put('/post/:postId', [
-    body('title').isLength({ min: 5 }).withMessage('input title min 5 karakter'),
-    body('body').isLength({ min: 5 }).withMessage('input content min 5 karakter')],
-    blogController.updateBlogPost);
+router.get("/post/:postId", blogController.getByIdBlogPost);
+router.put(
+  "/post/:postId",
+  [
+    body("title")
+      .isLength({ min: 5 })
+      .withMessage("input title min 5 karakter"),
+    body("body")
+      .isLength({ min: 5 })
+      .withMessage("input content min 5 karakter"),
+  ],
+  blogController.updateBlogPost
+);
 
-router.delete('/post/:postId', blogController.deleteBlogPost);
+router.delete("/post/:postId", blogController.deleteBlogPost);
 
 module.exports = router;
